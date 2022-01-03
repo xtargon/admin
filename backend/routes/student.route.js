a3d43d3e2bd28329a7eec8dc02e5913a0b7ea807
xtargon <70050390+xtargon@users.noreply.github.com>
2022-01-03 14:46:05 -0500

    Update student.route.js

--- backend/routes/student.route.js
+++ backend/routes/student.route.js
@@ -472,13 +472,13 @@ router.route('/updateStatus').get((req, res) => {
 
 });
 
- cron.schedule('0 33 14 * * *', () => {
+cron.schedule('0 7 * * *', () => {
    superagent.get('http://75.102.23.138:4000/students/updateStatus')
    console.log('Running a job at 07:00 at America/Mexico_City timezone');
  }, {
    scheduled: true,
-   timezone: "America/Lima"
- });
+   timezone: "America/Mexico_City"
+});
 
 function ensureToken(req, res, next){
   const bererHeader = req.headers['authorization'];
